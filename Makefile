4c119a953c168e82161585619ee3213a1d34a466
Kirill Pinchuk <cybergrind@gmail.com>
2015-12-22 18:15:24 +0300

    Optimize webpack.config

--- Makefile
+++ Makefile
@@ -6,4 +6,7 @@ npm_init:
 	npm install
 
 prod:
-	NODE_ENV=production npm run-script prod
+	NODE_ENV=production npm run-script build
+
+test:
+	NODE_ENV=development npm run-script build
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,55 @@
-var path = require('path');
-var webpack = require('webpack');
+'use strict';
+
+let path = require('path');
+let webpack = require('webpack');
+
+
+let ENV = process.env;
+let PRODUCTION = ENV.NODE_ENV == 'production';
+
+
+let devtool = PRODUCTION ? false : 'eval';
+let entry, plugins;
+
+if (PRODUCTION){
+    entry = ['./src/index'];
+    plugins = [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(ENV.NODE_ENV)
+            }}),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: false,
+            compressor: {
+                warnings: false,
+                drop_console: true,
+                screw_ie8: true
+            }
+        })
+    ];
+} else {
+    entry = [
+        'webpack-hot-middleware/client',
+        './src/index'
+    ];
+    plugins = [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(ENV.NODE_ENV)
+            }})
+    ];
+}
 
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-hot-middleware/client',
-    './src/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NoErrorsPlugin(),
-      new webpack.DefinePlugin({
-          'process.env': {
-              NODE_ENV: JSON.stringify('development')
-          }})
-  ],
+    devtool, entry, plugins,
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
     module: {
         loaders: [
             {test: /\.jsx?$/,
